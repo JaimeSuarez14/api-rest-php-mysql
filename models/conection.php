ec050845354424088cb6a43e8641b997313d6d54
Jaime Suarez <jaisua8@gmail.com>
2026-02-06 14:05:06 -0500

    creacion de la clase de conexion

--- models/conection.php
+++ models/conection.php
@@ -2,6 +2,8 @@
 
 class Conexion {
   static public function conectar() {
-    
+    $link = new PDO("mysql:host=localhost;dbname=api_rest_php", "root", "");
+    $link->exec("set names utf8");
+    return $link;
   }
 }
