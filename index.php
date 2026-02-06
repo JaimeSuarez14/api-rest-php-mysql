956a4f3a8c4d00441543e21eb84ec5cc913e44de
Jaime Suarez <jaisua8@gmail.com>
2026-02-06 14:14:07 -0500

    creacion de la modelos cursos

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 require_once "controllers/rutas.controllers.php";
 require_once "controllers/cursos.controllers.php";
 require_once "controllers/clientes.controllers.php";
+require_once "models/cursos.model.php";
+require_once "models/clientes.model.php";
 $rutas = new RutasControllers();
 $rutas->inicio();
---
+++ models/cursos.model.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "conection.php";
+
+class CursosModel {
+
+  public function getAll(){
+    $link = Conexion::conectar();
+    $query = $link->prepare("SELECT * FROM cursos");
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+    $query->close();
+    $link = null;
+  }
+}
